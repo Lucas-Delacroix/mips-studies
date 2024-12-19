1556a77a1b34bfb7046e92cd86cac0815a993dfa
Lucas-Delacroix <delacroixdev@gmail.com>
2024-12-19 15:30:24 -0300

    Atualiza quicksort

--- quicksort.asm
+++ quicksort.asm
@@ -1,42 +1,47 @@
-.data
-	array: 
-		.align 2 # Alinha palavra na posicao correta
-		.space 16
+.data 
+	array:
+		.align 2 # Alinha palavra na posicao correta, array de inteiros
+		.space 16 # Aloca 4 espa�os no array
 	space:
-		.asciiz " " #String contendo espaço em branco
-	newline:
+		.asciiz " " # String contendo um espa�o
+		
+	newline: 
 		.asciiz "\n" #Imprime quebra de linha
-
 .text
-	.global main
-
 main:
 	jal criarArray
-	jal ImprimirDesordenado
-	
+	jal imprimirArray
+	jal ordenarArray
+	jal imprimirArray	
 	li $v0, 10 # Código de saída
 	syscall
 
 	
+
+
 criarArray:
-	move $t0, $zero # Define indice do array como zero
-	li $t2, 16 #Define tamanho do array (16/4) elementos
+	move $t0, $zero #Define indice do array como zero
+	li $t2, 16 # Define Tamanho do array (16/4) elementos
 	loopCriarArray:
-		beq $t0, $t2, fimCriarArray #Se o indice for maior que a lista sai do loop
+		beq $t0, $t2, frimCriarArray #Se o indice for maior que a lista sai do loop
 		li $a1, 100 # Limite superior do intervalo de aleatorios
 		li $v0, 42 # C�digo para gerar inteiro aleatorio
 		syscall 
 		sw $a0, array($t0) # Salva o valor aleatorio gerado na posicao do array
 		addi $t0, $t0, 4 #incrementa indice do loop
 		j loopCriarArray
-	fimCriarArray:
+	frimCriarArray:
 		jr $ra
 
-ImprimirDesordenado:
+
+
+
+
+imprimirArray:
 	loopImprimirDesordenado:
-		move $t0, $zero #indice do array 
+		move $t0, $zero #indice do array  
 	imprimeDesordenado:
-		beq $t0, $t2, fimImprimeDesordenado #Se o indice for maior que a lista sai do loop
+		beq $t0, $t2, loopOrdenar #Se o indice for maior que a lista sai do loop
 		li $v0, 1 # Codigo para imprimir inteiro carregado
 		lw $a0, array($t0) # Argumento sendo valor do array na posicao
 		syscall
@@ -45,12 +50,47 @@ ImprimirDesordenado:
 		syscall
 		addi $t0, $t0, 4 # Incrementa indice do loop
 		j imprimeDesordenado
-	fimImprimeDesordenado:
-		li $v0, 4 #Imprime quebra de linha no final
+	fimImprimirDesordenado:
+		li, $v0, 4
 		la $a0, newline #Imprimir o \n
 		syscall
 		jr $ra #Retorna ao chamador
 		
 		
 		
+		
+		
+		
+ordenarArray:
+	loopOrdenar:
+		move $t3, $zero #indice do array
+	ordena:
+		beq $t3, $t2, fimLoopOrdenar #Se o indice for maior que a lista sai do loop
+		move $t4, $zero #Salva em t4 o indice do segundo loop
+	ordenaIterno:
+		addi $t5, $t4, 4 # Cria um indice com o valor +1 ao atual
+		beq $t5, $t2, fimInterno  # Se $t5 atingir o final do array, sai do loop interno
+		lw $t6, array($t4) #Valor do array na posicao 4
+		lw $t7, array($t5) #Valor do array na posicao 5
+		bgt $t6, $t7, troca #Se o atual for maior que o atual+1 vai para troca
+		addi $t4, $t4, 4# Incrementa �ndice interno
+		j ordenaIterno
+	fimInterno:
+		addi $t3, $t3, 4 # Incrementa o �ndice externo
+		j ordena
+	troca:
+		xor $t6, $t6, $t7
+		xor $t7, $t6, $t7
+		xor $t6, $t6, $t7
+		sw $t6, array($t4)# Salva o valor atualizado em $t4
+		sw $t7, array($t5)# Salva o valor atualizado em $t5
+		j ordenaIterno
+	fimLoopOrdenar:
+		li, $v0, 4
+		la $a0, newline #Imprimir o \n
+		syscall
+		jr $ra #Retorna ao chamador
 	
+	   	
+	   	
+		   
